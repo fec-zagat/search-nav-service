e64c4c0ba1f83a93e7cd74f14223be1a3866c070
Tyler Lindow <tyler.lindow@gmail.com>
2019-03-28 15:15:07 -0700

    Configure eslint for airbnb, react, jest, & babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,5 @@
-/**
- * These rules enforce the Hack Reactor Style Guide
- *
- * Visit this repo for more information:
- *   https://github.com/reactorcore/eslint-config-hackreactor
- */
-
 module.exports = {
-  extends: './node_modules/eslint-config-hackreactor/index.js'
+  "extends": ["airbnb-base", 'plugin:react/recommended'],
+  "env": {"jest": true},
+  "parser": "babel-eslint",
 };
