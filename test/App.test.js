996cfba9373f35d22a60828fb88351ff8168f19f
Tyler Lindow <tyler.lindow@gmail.com>
2019-04-01 09:45:10 -0700

    Change test to reflect addition of SearchBar

--- test/App.test.js
+++ test/App.test.js
@@ -4,7 +4,7 @@ import App from '../client/components/App.jsx';
 
 describe('App component build test', () => {
   const wrapper = shallow(<App />);
-  test('Rendered app displays "App is built and served"', () => {
-    expect(wrapper.text()).toEqual('App is built and served');
+  test('Rendered app displays <SearchBar />', () => {
+    expect(wrapper.text()).toEqual('<SearchBar />');
   });
 });
