384c318c2a2fd34c127570165d8e4a7e48ef5054
Tyler Lindow <tyler.lindow@gmail.com>
2019-04-01 09:43:13 -0700

    Initialize form setup for search bar component

---
+++ client/components/SearchBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SearchBar = () => (
+  <nav>
+    <h1>TAGAZ</h1>
+    <form>
+      <input placeholder="Find a great place near you"></input>
+    </form>
+    <ul>
+      <li>The Latest</li>
+      <li>City&apos;s Best</li>
+      <li>San Francisco</li>
+    </ul>
+  </nav>
+
+);
+
+export default SearchBar;
