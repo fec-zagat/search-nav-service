f2a3967ad56e5ae4f91f1328dc73aa97773e4c45
Tyler Lindow <tyler.lindow@gmail.com>
2019-04-08 16:24:53 -0700

    Change get route to '/search/r/query'
    Made this change to avoid conflicts with other /r/restaurant requests

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const dbMethods = require('./db/dbMethods.js');
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 const port = 3000;
 
-app.use(express.static('public'));
+app.use('/r/:restaurant', express.static('public'));
 
-app.get('/r/:query', (req, res) => {
+app.get('/search/r/:query', (req, res) => {
   mongoose.connect(`mongodb://tlindow:${process.env.dbpassword}@${process.env.devDB}`, { useNewUrlParser: true }, () => {
     dbMethods.getByQuery(req.params.query, (err, restaurants) => {
       if (err) {
@@ -23,6 +25,9 @@ app.get('/r/:query', (req, res) => {
   });
 });
 
+app.get('*', (req, res) => {
+  res.redirect('/r/eoss-oharahaven/');
+});
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
