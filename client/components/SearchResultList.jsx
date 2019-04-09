3257843e6636f497ffa661856a49c52b746e3582
Tyler Lindow <tyler.lindow@gmail.com>
2019-04-08 17:05:24 -0700

    Pass down restaurant information to SearchResult

--- client/components/SearchResultList.jsx
+++ client/components/SearchResultList.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import SearchResult from './SearchResult.jsx';
+import '../styles/searchbar.css';
 
 const SearchResultList = props => (
-  <div>
+  <div className="search-bar-dropdown-container">
     {props.restaurants.map((restaurant, idx) => (
-      <SearchResult key={idx} restaurant={restaurant}/>
+      <SearchResult key={idx}
+                    restaurant={restaurant}
+                    goToRestaurantPage={props.goToRestaurantPage}
+      />
     ))}
   </div>
 );
 
 SearchResultList.propTypes = {
   restaurants: propTypes.array.isRequired,
+  goToRestaurantPage: propTypes.func.isRequired,
 };
 
 export default SearchResultList;
