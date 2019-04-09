a9e20e8f8c2ab327052bed6246fe184883321bdb
Tyler Lindow <tyler.lindow@gmail.com>
2019-04-08 17:26:08 -0700

    Change test to reflect update in search route

--- test/dbMethods.test.js
+++ test/dbMethods.test.js
@@ -1,11 +1,11 @@
 const request = require('supertest');
 const app = require('../index.js');
 
-describe('GET /r/:query ', () => {
+describe('GET /search/r/:query ', () => {
   it('responds with json', (done) => {
     const testQuery = 'abc';
     request(app.listen())
-      .get(`/r/${testQuery}`)
+      .get(`/search/r/${testQuery}`)
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, done);
