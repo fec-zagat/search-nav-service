c6a37c2270453e3aec905ad2436e42071ee483ac
Tyler Lindow <tyler.lindow@gmail.com>
2019-04-10 14:45:15 -0700

    Create file to store list of cuisines

---
+++ client/utils/cuisineList.js
@@ -0,0 +1,19 @@
+const cuisines = [
+  'american',
+  'brunch',
+  'burger',
+  'chinese',
+  'french',
+  'indian',
+  'italian',
+  'japanese',
+  'mexican',
+  'pizza',
+  'seafood',
+  'sushi',
+  'thai',
+  'vegetarian',
+  'vietnamese',
+];
+
+export default cuisines;
