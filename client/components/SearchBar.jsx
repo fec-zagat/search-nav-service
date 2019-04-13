c28ac4c6cff9a8a606a9af73fc7f96e1b7769efb
Tyler Lindow <tyler.lindow@gmail.com>
2019-04-13 15:20:20 -0700

    Conditionally render Cuisines or SearchResultList

--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import propTypes from 'prop-types';
 import SearchResultList from './SearchResultList.jsx';
 import SearchBarLocation from './SearchBarLocation.jsx';
+import SearchBarCuisine from './SearchBarCuisine.jsx';
 import '../styles/searchbar.css';
+import cuisines from '../utils/cuisineList';
 
 class SearchBar extends React.Component {
   constructor(props) {
@@ -38,15 +40,27 @@ class SearchBar extends React.Component {
             { this.props.showSearchResults
               ? <div className="search-result-lists">
                     <SearchResultList restaurants={this.props.restaurants}
+                                      restaurantImages={this.props.restaurantImages}
+                                      showSuggestion={this.props.showSuggestion}
+                                      showSuggestionStyle={this.props.showSuggestionStyle}
+                                      showSearchResults={this.props.showSearchResults}
                                       goToRestaurantPage={this.props.goToRestaurantPage}
                     />
-                </div> : null}
+                </div>
+              : <div className="search-bar-dropdown-cuisines">
+              { cuisines.map((cuisine, idx) => (
+                <SearchBarCuisine key={idx} cuisine={cuisine}/>
+              )) }
+            </div>
+              }
               </div>
               <SearchBarLocation restaurants={this.props.restaurants}
                         goToRestaurantPage={this.props.goToRestaurantPage}
                         handleSearchBarLocationMount={this.handleSearchBarLocationMount.bind(this)}
               />
             </div>
+
+
           : <div className="search-bar-nav-container-infocus">
               <div className="search-bar-container">
                 <div className="search-icon"></div>
@@ -60,6 +74,10 @@ class SearchBar extends React.Component {
           { this.props.showSearchResults
             ? <div className="search-result-lists">
                 <SearchResultList restaurants={this.props.restaurants}
+                                  restaurantImages={this.props.restaurantImages}
+                                  showSearchResults={this.props.showSearchResults}
+                                  showSuggestion={this.props.showSuggestion}
+                                  showSuggestionStyle={this.props.showSuggestionStyle}
                                   goToRestaurantPage={this.props.goToRestaurantPage}
                 />
               </div> : null}
@@ -72,9 +90,12 @@ class SearchBar extends React.Component {
 SearchBar.propTypes = {
   handleSearch: propTypes.func.isRequired,
   restaurants: propTypes.array.isRequired,
+  restaurantImages: propTypes.array.isRequired,
   goToRestaurantPage: propTypes.func.isRequired,
   showSearchResults: propTypes.bool.isRequired,
   showSearchBarDropdown: propTypes.func.isRequired,
+  showSuggestion: propTypes.bool.isRequired,
+  showSuggestionStyle: propTypes.string.isRequired,
 };
 
 export default SearchBar;
