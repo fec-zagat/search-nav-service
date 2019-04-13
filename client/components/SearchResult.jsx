20a3639c5effad51d080731503118ae5d7c61163
Tyler Lindow <tyler.lindow@gmail.com>
2019-04-13 15:14:02 -0700

    Add loading bars while suggest results rendering

--- client/components/SearchResult.jsx
+++ client/components/SearchResult.jsx
@@ -3,13 +3,23 @@ import propTypes from 'prop-types';
 import '../styles/navbar.css';
 
 const SearchResult = props => (
-  <div className="search-bar-place-suggestion" onClick={() => props.goToRestaurantPage(props.restaurant)}>
-    <img className="search-bar-place-suggestion-image" src={props.restaurant.image_url}></img>
+  <div>
     <div>
-      <div className="search-bar-place-suggestion-title">{props.restaurant.name}
-        <div className="search-bar-place-icon-logo"></div>
+    {/* Gray bars when search result rendering */}
+    { props.showSuggestionStyle === 'hide'
+      ? <div className="search-suggestion-loading-bars">
+        </div>
+      : null }
+    </div>
+
+    <div className={`search-bar-place-suggestion-${props.showSuggestionStyle}`} onClick={() => props.goToRestaurantPage(props.restaurant)}>
+      <img className="search-bar-place-suggestion-image" src={props.restaurant.image_url}></img>
+      <div>
+        <div className="search-bar-place-suggestion-title">{props.restaurant.name}
+          <div className="search-bar-place-icon-logo"></div>
+        </div>
+        <div className="search-bar-place-suggestion-address">{props.restaurant.address.street}</div>
       </div>
-      <div className="search-bar-place-suggestion-address">{props.restaurant.address.street}</div>
     </div>
   </div>
 );
@@ -17,6 +27,8 @@ const SearchResult = props => (
 SearchResult.propTypes = {
   restaurant: propTypes.object.isRequired,
   goToRestaurantPage: propTypes.func.isRequired,
+  showSuggestion: propTypes.bool.isRequired,
+  showSuggestionStyle: propTypes.string.isRequired,
 };
 
 export default SearchResult;
